14ad2396b7196aee1995c896a4dc9f9f1adf4486
mdgaziur001 <mdgaziurrahmannoor@gmail.com>
2020-11-16 11:28:58 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { v4 } = require('uuid');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send(`
+    <center>
+        <h1>Audio Streaming</h1>
+        <a href="/sender">Send audio</a><br/>
+        <a href="/receiver">Receive audio</a>
+    </center>
+    `)
+});
+
+app.get('/sender', (req, res) => {
+    res.render("sender", {
+        ID: v4()
+    });
+    res.end();
+});
+
+app.get('/receiver', (req, res) => {
+    res.render("receiver", {
+        ID: v4()
+    });
+    res.end();
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomid, peerid, type) => {
+        socket.join(roomid);
+        socket.to(roomid).broadcast.emit(type === 1 ? "sender-connected" : "receiver-connected", peerid);
+
+        socket.on('disconnect', () => {
+            socket.to(roomid).broadcast.emit(type === 1 ? "sender-disconnected" : "receiver-disconnected", peerid);
+        });
+    });
+});
+
+server.listen(3000);
